ce2ca5899795c59d3c307345a090e06a7828901a
Leandro Giordano <40839984+leangior@users.noreply.github.com>
2025-09-01 17:14:42 -0300

    Update CrosCorrAnalysys.py

--- CrosCorrAnalysys.py
+++ CrosCorrAnalysys.py
@@ -3,7 +3,7 @@ import numpy as np
 from statsmodels.api import OLS, add_constant
 from typing import Optional, Union, List, Tuple
 
-def get_cross_cor(up_series, down_series, max_lag=10, ini=1)-> pd.DataFrame:
+def get_cross_cor(up_serie, down_serie, max_lag=10, ini=1)-> pd.DataFrame:
     """Evalúa la correlación cruzada entre una señal de aguas arriba (up_serie) y una señal de aguas abajo (down_serie)
     
     Args:
@@ -20,9 +20,9 @@ def get_cross_cor(up_series, down_series, max_lag=10, ini=1)-> pd.DataFrame:
     """
     r = []
     for i in range(ini, max_lag + 1):
-        u_lagged = up_series.shift(i)
-        df = pd.concat([u_lagged, down_series], axis=1).dropna()
-        correlation = df.corr().iloc[1, 0]  # Correlation between lagged up_series and down_series
+        u_lagged = up_serie.shift(i)
+        df = pd.concat([u_lagged, down_serie], axis=1).dropna()
+        correlation = df.corr().iloc[1, 0]  # Correlation between lagged up_series and down_serie
         r.append(correlation)
     return pd.DataFrame(r,index=range(ini, max_lag + 1))
 
@@ -43,11 +43,36 @@ def get_response_time(up_serie : Union[pd.Series,pd.DataFrame], down_serie : Uni
     r=get_cross_cor(up_serie,down_serie,max_lag,ini).idxmax()
     return(int(r[0]))
 
-def get_lag_and_linear_fit(up_series : Union[pd.Series,pd.DataFrame], down_serie : Union[pd.Series,pd.DataFrame], max_lag : int =10,ini : int =1, verbose : bool = True) -> List:
-    """Evalúa la correlación cruzada entre una señal de aguas arriba (up_serie) y una señal de aguas abajo (down_serie), determina el tiempo de traslado màs adecuado para la hipótesis de asociación lineal y devuelve el modelo lineal correspondiente y el dataframe de predictores
+def shifted_series_by_best_lag(up_series : Union[pd.Series,pd.DataFrame], down_serie : Union[pd.Series,pd.DataFrame], max_lag : int =10,ini : int =1, freq : str = "infer", verbose : bool = True) -> pd.DataFrame:
+        """Evalúa la correlación cruzada entre una señal o conjunto de señales de aguas arriba (up_serie) y una señal de aguas abajo (down_serie), determina el tiempo de traslado más adecuado para la hipótesis de asociación lineal en cada caso y devuelve el dataframe de predictores - desplazados de acuerdo a sus correspondientes retardos -
     Args:
         up_serie :  
-            serie regularizada 'aguas arriba'
+            serie o series regularizada 'aguas arriba'
+        down_serie :  
+            serie regularizada 'aguas abajo' 
+        ini : 
+            retardo inicial
+        max_lag: 
+            máximo retardo
+        Returns:
+            Devuelve un dataframe con las series de aguas arriba desplazadas por su correspondiente lag óptimo 
+    """
+        X=pd.DataFrame()
+        for i in range(0,up_series.shape[1]):
+            best_lag = get_response_time(up_series.iloc[:,i], down_serie, max_lag,ini) 
+            if verbose == True :
+                print("Serie X"+str(i)+" Best Lag: "+str(best_lag)+" steps")    
+            up_serie_shifted = up_series.iloc[:,i].shift(best_lag,freq)
+            X = pd.concat([X,up_serie_shifted], axis=1)
+            X.index=pd.to_datetime(X.index)
+        
+        return X
+
+def get_lag_and_linear_fit(up_series : Union[pd.Series,pd.DataFrame], down_serie : Union[pd.Series,pd.DataFrame], max_lag : int =10,ini : int =1, verbose : bool = True) -> List:
+    """Evalúa la correlación cruzada entre una señal o conjunto de señales de aguas arriba (up_series) y una señal de aguas abajo (down_serie), determina el tiempo de traslado más adecuado para la hipótesis de asociación lineal em cada caso y devuelve el modelo lineal correspondiente (univariante/multivariante) y el dataframe de predictores - desplazados de acuerdo a sus correspondientes retardos óptimos-
+    Args:
+        up_serie :  
+            serie o series regularizada/s 'aguas arriba'
         down_serie :  
             serie regularizada 'aguas abajo' 
         ini : 
@@ -57,24 +82,20 @@ def get_lag_and_linear_fit(up_series : Union[pd.Series,pd.DataFrame], down_serie
         Returns:
             Devuelve una lista con el modelo lineal para el cual se maximiza la correlación cruzada y con el dataframe de predictores 
     """
-    X=pd.DataFrame()
 
-    for i in range(0,up_series.shape[1]):
-        best_lag = get_response_time(up_series.iloc[:,i], down_serie, max_lag,ini) 
-        if verbose == True :
-            print("Serie X"+str(i)+" Best Lag: "+str(best_lag)+" steps")    
-        up_serie_shifted = up_series.iloc[:,i].shift(best_lag)
-        X = pd.concat([X,up_serie_shifted], axis=1)
-
+    X = shifted_series_by_best_lag(up_series,down_serie,max_lag,ini)
     X = pd.concat([X,down_serie],axis=1).dropna()
     y = X.iloc[:,X.shape[1]-1] 
     X = X.iloc[:,0:X.shape[1]-1]
     X = add_constant(X)
     model = OLS(y, X).fit()
+    if verbose == True:
+         print("Model Adjusted R Square = " + str(model.rsquared))
     return [model,X]
+
     
 def shift_and_adjust(up_series : Union[pd.Series,pd.DataFrame], down_serie : Union[pd.Series,pd.DataFrame], max_lag : int =10,ini : int =1) -> pd.DataFrame:
-    """Evalúa la correlación cruzada entre una señal de aguas arriba (up_serie) y una señal de aguas abajo (down_serie), determina el tiempo de traslado màs adecuado para la hipótesis de asociación lineal, obtiene el modelo lineal correspondiente y lo ejecuta
+    """Evalúa la correlación cruzada entre una señal de aguas arriba (up_serie) y una señal de aguas abajo (down_serie), determina el tiempo de traslado más adecuado para la hipótesis de asociación lineal, obtiene el modelo lineal correspondiente y lo ejecuta. Devuelve el vector de valores estiamdos/previstos. 
     Args:
         up_serie :  
             serie regularizada 'aguas arriba'
@@ -91,6 +112,42 @@ def shift_and_adjust(up_series : Union[pd.Series,pd.DataFrame], down_serie : Uni
     coef = model[0].params.to_numpy()
     sim = model[1].dot(coef)
     return sim
+
+def execute_model(pred_serie : Union[pd.Series,pd.DataFrame], model : Union[pd.DataFrame,np.ndarray] = pd.DataFrame(), obs_serie : Union[pd.Series,pd.DataFrame] = pd.DataFrame(), max_lag : int = 10,ini : int = 1) -> pd.DataFrame:
+    """En el caso maś simple se proveen predictores y un modelo (objeto OLS) y lo ejecuta. En este caso no se realiza ningún análisis sobre la base de la serie de predictores (esta se considera como tal, se asume que ya fueron determinados lags y transformaciones óptimas y que el modelo provisto considera esto). En efecto, en este caso simplemente realiza el producto de matrices. En caso que se provea una serie observada determina los lags óptimos y ejecuta el modelo provisto. En caso que se provea una serie observada y no se provea un modelo, realiza el análisis correspondiente, determina lag óptimos, coeficientes de ajuste y ejecuta el modelo obtenido.  
+    Args:
+        pred_serie :  
+            data frame con predictores (vectores columna)
+        pars :  
+            data frame coeficientes objeto OLS  
+        obs_serie : 
+            serie observada (opcional)
+        Returns:
+            Devuelve data frame con valores estimados y observados (opcional)
+    """ 
+
+    if hasattr(model,'params'):
+         coef = model.params.to_numpy()
+    elif model.empty == True:
+        if obs_serie.empty == True:
+            raise NameError('No model can be found if no obs serie is provided. Check input data')
+        else:
+            model = get_lag_and_linear_fit(pred_serie,obs_serie,max_lag,ini)
+            coef = model[0].params.to_numpy()
+         
+    if obs_serie.empty == True:
+        pred_serie = add_constant(pred_serie)
+        sim = pred_serie.dot(coef)
+        sim.columns=['sim']
+    else :
+        pred_serie = shifted_series_by_best_lag(pred_serie,obs_serie,max_lag,ini)
+        pred_serie = add_constant(pred_serie) 
+        sim = pred_serie.dot(coef)
+        sim=pd.concat([sim,obs_serie],axis=1)
+        sim.columns=['sim','obs']
+    
+    return sim
+
 
 if __name__ == "__main__":
     import sys
